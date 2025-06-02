1cdb817b18c15803b2aeaaeb13e398f291ec9e89
DLeafGreen <66260675+saidlab@users.noreply.github.com>
2025-06-02 14:36:14 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,17 @@
-test() {
-  echo $1
-}
+# concat_args
+A() { local X=''; for Y in "$@"; do X+="$Y"; done; echo "$X"; }
+
+# convert_string
+B() { printf "%b\n" "$1"; }
+
+# convert_function
+C() { "$@" > /dev/null 2>&1; }
+
+# verify_root
+D() { [ "$EUID" -eq 0 ]; }
+
+# verify_adb
+E() { command -v pm >/dev/null 2>&1; }
+
+# hex_encode
+F() { echo -n "$1" | xxd -r -p; }
